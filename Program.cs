a3e54e24b2e05449df69f073e3cd448d7bd9a45a
JMaycol-Valencia <maycolvalencia50@gmail.com>
2024-12-17 17:10:01 -0400

    connection-to-data-base

--- Program.cs
+++ Program.cs
@@ -1,7 +1,19 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using projectef;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSqlServer<TareasContext>(builder.Configuration.GetConnectionString("TareasDbConnectionOne"));
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
+app.MapGet("/dbconexion", async ([FromServices] TareasContext dbContext) => 
+{
+    dbContext.Database.EnsureCreated();
+    return Results.Ok("Base de datos en Memoria:" + dbContext.Database.IsSqlServer());
+});
 
 app.Run();
